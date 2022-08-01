a1845afa2e6edf9b1e77f7011473ff8c7d90bffc
NehaWargantiwar <110392680+NehaWargantiwar@users.noreply.github.com>
2022-08-01 18:03:01 +0530

    by git team

--- Automation Testing GIT/src/Git_project/DemoGit.java
+++ Automation Testing GIT/src/Git_project/DemoGit.java
@@ -2,6 +2,18 @@ package Git_project;
 
 public class DemoGit 
 {
+	
+	public void m1 ()
+	{
+		System.out.println("m1 start");
+		
+	}
+	
+	public void m2 ()
+	{
+		System.out.println("m2 start");
+		
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("GIT");
